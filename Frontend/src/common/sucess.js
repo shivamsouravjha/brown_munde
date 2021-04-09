e6b02306f5ade233c99eed286e779bfcf2e4bdaf
Priyansh Nama <54625644+priyanshnama@users.noreply.github.com>
2021-04-09 23:19:00 +0530

    other files added

---
+++ Frontend/src/common/sucess.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+const  Driver_login = ()=>{
+    return (   
+        <h2>nice </h2>
+
+    );
+  
+};
+ 
+export default Driver_login;
