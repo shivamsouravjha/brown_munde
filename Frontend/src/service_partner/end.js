43c0019fdf230b30b87937ddfd7b04eae0e218bb
Priyansh Nama <54625644+priyanshnama@users.noreply.github.com>
2021-04-09 22:18:49 +0530

    frontend added

---
+++ Frontend/src/service_partner/end.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+
+const  End = ()=>{
+    const uid = useParams().uid;
+
+    console.log(uid);
+    const availability = async e =>{
+        console.log("did");
+
+        e.preventDefault();
+        try{
+           
+            const response = await fetch(`https://taxibackendf.herokuapp.com/api/booking/end/${uid}`,{
+                method:"POST",headers:{"Content-Type":"application/json"}
+            })
+            const datainjson = await response.json();
+            window.location =`/success`;
+
+        }catch(err){
+            console.log('Error')
+        }
+
+    }
+
+    return (   
+<div className="admin_form_div">
+		<h1 className="form_header">
+			Hey Admin! </h1>
+	
+  <center>
+	<form  action="/initial" id="admin_form"  name="admin_form" onSubmit={availability}>
+  			<button type="submit" className="confirm_btn" >Confirm</button>
+        </form> 
+        </center>
+
+
+
+
+    </div>    );
+  
+};
+ 
+export default End;
