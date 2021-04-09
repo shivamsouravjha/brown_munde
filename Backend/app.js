aab55d18cbb4abc505cb801dd53d09d342c39f4f
Shrestha25 <25shrestha97@gmail.com>
2021-04-09 22:46:57 +0530

    All the backend starters

---
+++ Backend/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const service_router = require('./ZRoutes/service_router');
+///const driver_router = require('./ZRoutes/driver_router');
+////const booking_routers = require('./ZRoutes/booking_routers');
+const Erur = require('./Service_Model_stage2/Error');
+const cors = require('cors')
+
+const app = express();
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use('/api/service', service_router); // => /api/places...
+////app.use('/api/driver', driver_router);
+///pp.use('/api/booking/', booking_routers);
+
+app.use((req, res, next) => {
+    const error = new Erur('Could not find this route.', 404);
+    throw error;
+  });
+  
+  app.use((error, req, res, next) => {
+    if (res.headerSent) {
+      return next(error);
+    }
+    res.status(error.code || 500)
+    res.json({message: error.message || 'An unknown error occurred!'});
+  });
+  mongoose
+  .connect(
+    `mongodb://${process.env.name}:${process.env.password}@cluster0-shard-00-00.dm1xw.mongodb.net:27017,cluster0-shard-00-01.dm1xw.mongodb.net:27017,cluster0-shard-00-02.dm1xw.mongodb.net:27017/${process.env.db}?ssl=true&replicaSet=atlas-x6eag6-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true }
+  )
+  .then(() => {
+    app.listen(process.env.PORT || 5000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
