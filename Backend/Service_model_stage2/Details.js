e9d16ad5950362dace3db452c1d18213d5f8dc88
Shrestha25 <25shrestha97@gmail.com>
2021-04-09 23:22:01 +0530

    Add details and error file

---
+++ Backend/Service_model_stage2/Details.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const schema =  mongoose.Schema;
+const Details_schema = new schema({
+        service_id: {type :mongoose.Types.ObjectId,required:true,ref:'Loggin_schema',unique:true },
+        location_of_work: {type: String,required:true}, ///3 fields->All_India , Across_State, Local_Guy
+        education:{type:String,required:true}, ///option-a,b,c,d,e,f
+        emergency_name:{ type: String, required: true},
+        phone: {type :String,minlength:10,maxLength:10 },
+        emergency_relation:{ type: String, required: true},
+        emergency_email:{ type: String, required: true},
+        experience:{ type: String},
+        rate:{ type: String, required: true},
+        aadhar_number: { type: String, required: true,unique:true,minlength:12,maxLength:12 },
+        skill:{ type: String, required: true}
+});
+
+module.exports = mongoose.model('Details',Details_schema);
